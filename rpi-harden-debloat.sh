20621be1204ce4193a4ed886499c97f2301a964a
Rohitrajak1807 <rohit.rajakah11@gmail.com>
2021-04-16 00:11:18 +0530

    add, enforce apparmor fix sudoers file

--- rpi-harden-debloat.sh
+++ rpi-harden-debloat.sh
@@ -14,7 +14,7 @@ remove_packages() {
     apt-get clean -y
 }
 
-diasable_services() {
+disable_services() {
     local services=(sshd ssh avahi-daemon cups.service cups)
     for service in "${services[@]}"; do
         systemctl disable --now "${service}"
@@ -30,16 +30,44 @@ remove_from_groups() {
 
 setup_ufw() {
     apt-get install ufw -y
+    ufw enable
     ufw default deny incoming
     # if SSH is needed enable rate limiting
     # ufw allow ssh
     # ufw limit ssh
 }
 
-# TODO add ufw and configure
+setup_apparmor() {
+    apt-get install apparmor-profiles apparmor-utils -y
+    aa-enforce /etc/apparmor.d/*
+}
+
+disable_usbhid_devices() {
+    echo 'blacklist usbhid' > /etc/modprobe.d/usbhid-blacklist.conf
+    update-initramfs -u
+}
+
+update_and_upgrade() {
+    apt-get update -y
+    apt-get upgrade -y
+    apt-get dist-upgrade -y
+}
+
+fix_sudoers() {
+    touch /tmp/sudoers.tmp
+    cp /etc/sudoers /tmp/sudoers.tmp
+    sed -i "s/ALL\:ALL/ALL/g" /tmp/sudoers.tmp
+    visudo -c -f /tmp/sudoers.tmp
+    cp /tmp/sudoers.tmp /etc/sudoers
+    rm /tmp/sudoers.tmp
+}
 
 check_process_privileges
 remove_packages
-diasable_services
+disable_services
+update_and_upgrade
 setup_ufw
+setup_apparmor
+disable_usbhid_devices
+fix_sudoers
 remove_from_groups
